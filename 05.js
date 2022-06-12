9a37cbef2f1bf0032a421cd483985d734e67441c
weilunh7 <99935037+weilunh7@users.noreply.github.com>
2022-06-12 21:42:46 +0800

    Delete 05.js

--- 05.js
+++
@@ -1,9 +0,0 @@
-function answer(number) {
-    var sum = 0,Length = number.length;
-    for(let i=0 ; i < Length ; i++) {
-        sum = sum + number[i];
-}
-    return sum / Length;
-}
-var number = [1,2,3,4,5];
-console.log(answer(number))
